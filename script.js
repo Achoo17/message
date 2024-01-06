093350db94849dfe7daeb783432bf35ec1cbe0d7
Achoo17 <155817098+Achoo17@users.noreply.github.com>
2024-01-06 17:02:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Lol!! Stupid";
+  question.innerHTML = "Lol!!!";
   gif.src =
     "https://media.giphy.com/media/zcMpjgfqZo0Le/giphy.gif";
 });
